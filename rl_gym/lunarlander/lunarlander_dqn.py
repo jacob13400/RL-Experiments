282eb848d91f3aedb3ae43ed715dc0aa8135e4be
Jacob Abraham <jacob13400@cet.ac.in>
2022-05-04 23:20:17 +0530

    New environment added

--- rl_gym/lunarlander/lunarlander_dqn.py
+++ rl_gym/lunarlander/lunarlander_dqn.py
@@ -17,8 +17,10 @@ from collections import deque
 # Parameter updates are made via Adam.
 
 env_to_use = 'LunarLander-v2'
+env_to_use_2 = 'LunarLanderContinuous-v2'
 
 # hyperparameters
+target_episodes = 5
 gamma = 0.99				# reward discount factor
 h1 = 12					# hidden layer 1 size
 h2 = 12					# hidden layer 2 size
@@ -27,7 +29,7 @@ lr = 5e-5				# learning rate
 lr_decay = 1			# learning rate decay (per episode)
 l2_reg = 1e-6				# L2 regularization factor
 dropout = 0				# dropout rate (0 = no dropout)
-num_episodes = 1000		# number of episodes
+num_episodes = 10		# number of episodes
 max_steps_ep = 10000	# default max number of steps per episode (unless env has a lower hardcoded limit)
 slow_target_burnin = 1000		# number of steps where slow target weights are tied to current network weights
 update_slow_target_every = 100	# number of steps to use slow target as target before updating it to latest weights
@@ -44,13 +46,17 @@ env = gym.make(env_to_use)
 state_dim = np.prod(np.array(env.observation_space.shape)) 	# Get total number of dimensions in state
 n_actions = env.action_space.n 								# Assuming discrete action space
 
+env_2 = gym.make(env_to_use_2)
+
 # set seeds to 0
 env.seed(0)
+env_2.seed(0)
 np.random.seed(0)
 
 # prepare monitorings
-outdir = '/home/jake/Desktop/Work/RL-Experiments/tmp/dqn-agent-results3'
+outdir = '/home/jake/Desktop/Work/RL-Experiments/tmp/dqn-agent-results'
 env = wrappers.Monitor(env, outdir, force=True)
+env_2 = wrappers.Monitor(env, outdir, force=True)
 def writefile(fname, s):
     with open(path.join(outdir, fname), 'w') as fh: fh.write(s)
 info = {}
@@ -163,6 +169,7 @@ for ep in range(num_episodes):
 
 	# Initial state
 	observation = env.reset()
+	observation = env_2.reset()
 	# env.render()
 
 	for t in range(max_steps_ep):
@@ -176,8 +183,12 @@ for ep in range(num_episodes):
 			action = np.argmax(q_s)
 
 		# take step
-		next_observation, reward, done, _info = env.step(action)
-		# env.render()
+		if (ep < target_episodes):
+			next_observation, reward, done, _info = env.step(action)
+			env.render()
+		else:
+			next_observation, reward, done, _info = env_2.step(action)
+			env_2.render()
 		total_reward += reward
 
 		# add this to experience replay buffer
